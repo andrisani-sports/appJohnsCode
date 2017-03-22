47e2a3b337a6e8f96434c0be6a32d2fc5a9faad7
steveroettger <steve@steveroettger.com>
2017-03-22 11:50:09 -0400

    added modals

--- www/js/appController.js
+++ www/js/appController.js
@@ -1,12 +1,15 @@
 (function(gScope){
 
 angular.module(gScope.AppNameId)
-.controller('AppController', ['$log', '$scope','fhsCordova','$rootScope', function($log, $scope, fhsCordova, $rootScope){
+.controller('AppController', ['$log', '$scope','fhsCordova','$rootScope', 'SharedState', function($log, $scope, fhsCordova, $rootScope, SharedState){
 		var app = this;
 
 		$rootScope.chosenPitcher = {
 			name: ''
 		};
+
+		SharedState.initialize($scope, 'modal1');
+    SharedState.initialize($scope, 'modal2');
 
  		$scope.$on(fhsCordova.RESUME, resume);
 		$scope.$on(fhsCordova.PAUSE, pause);
